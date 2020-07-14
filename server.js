57153655da666e52bc9596e60f826ff13ce801dd
Jacob Corum <cor15031@byui.edu>
2020-07-13 18:18:06 -0600

    put request added to party table

--- server.js
+++ server.js
@@ -4,15 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const inputCheck = require("./utils/inputCheck");
 
-// middleware
-app.use(express.urlencoded({
-    extended: false
-}));
-
-app.use(express.json());
-
-
-// SQL queries
+// connect to database
 const db = new sqlite3.Database("./db/election.db", err => {
     if (err) {
         return console.error(err.message);
@@ -21,8 +13,21 @@ const db = new sqlite3.Database("./db/election.db", err => {
     console.log("Connected to the election database");
 });
 
-// GET all candidates
-app.get("/api/candidates", (req, res) => {
+// middleware
+app.use(express.urlencoded({
+    extended: false
+}));
+
+app.use(express.json());
+
+// get requests
+app.get("/", (req, res) => { // root route
+    res.json({
+        message: "Hello World!"
+    });
+});
+
+app.get("/api/candidates", (req, res) => { // GET all candidates
     const sql = `SELECT candidates.*, parties.name 
     AS party_name 
     FROM candidates 
@@ -44,9 +49,7 @@ app.get("/api/candidates", (req, res) => {
     });
 });
 
-
-// GET a single candidate
-app.get("/api/candidate/:id", (req, res) => {
+app.get("/api/candidate/:id", (req, res) => { // GET a single candidate
     const sql = `SELECT candidates.*, parties.name 
     AS party_name 
     FROM candidates 
@@ -69,9 +72,44 @@ app.get("/api/candidate/:id", (req, res) => {
     });
 });
 
+app.get("/api/parties", (req, res) => { // get all parties
+    const sql = `SELECT * FROM parties`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+            res.status(500).json({
+                error: err.message
+            });
+            return;
+        }
 
-// DELETE a candidate
-app.delete("/api/candidate/:id", (req, res) => {
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
+});
+
+app.get("/api/parties/:id", (req, res) => { // get one party
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, rows) => {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
+            return;
+        }
+
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
+});
+
+// DELETE requests
+app.delete("/api/candidate/:id", (req, res) => { // DELETE a candidate
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
     db.run(sql, params, function (err, result) {
@@ -89,10 +127,26 @@ app.delete("/api/candidate/:id", (req, res) => {
     });
 });
 
+app.delete("/api/party/:id", (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({
+                error: res.message
+            });
+            return;
+        }
 
-// create a candidate 
-// Create a candidate
-app.post('/api/candidate', ({
+        res.json({
+            message: "successfully deleted",
+            changes: this.changes
+        })
+    })
+})
+
+// POST requests
+app.post('/api/candidate', ({ // Create a candidate
     body
 }, res) => {
     const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
@@ -122,15 +176,35 @@ app.post('/api/candidate', ({
     });
 });
 
-app.use((req, res) => { // Default response for any other request(Not Found) Catch all
-    res.status(404).end();
+// PUT requests (for modifying existing data)
+app.put("/api/candidate/:id", (req, res) => {
+    const errors = inputCheck(req.body, "party_id");
+    if (errors) {
+        res.status(400).json({
+            error: errors
+        });
+        return;
+    }
+    const sql = `UPDATE candidates SET party_id = ? WHERE id = ?`;
+    const params = [req.body.party_id, req.params.id];
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({
+                error: err.message
+            });
+            return;
+        }
+
+        res.json({
+            message: "success",
+            data: req.body,
+            changes: this.changes
+        });
+    });
 });
 
-// get routes
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hello World!"
-    });
+app.use((req, res) => { // Default response for any other request (Not Found) Catch all
+    res.status(404).end();
 });
 
 // run server (used event listener to make sure server goes live after it connects to the database)
